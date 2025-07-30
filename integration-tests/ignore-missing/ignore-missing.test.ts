552d9802abe7e2ece22721978d6bdc960ea678aa
Naandalist <listiananda.apriliawan@gmail.com>
2025-07-31 06:58:45 +0700

    chore: bumpup version to 8.1.5

--- integration-tests/ignore-missing/ignore-missing.test.ts
+++
@@ -1,5 +0,0 @@
-import { runIntegrationTest } from "../runIntegrationTest"
-runIntegrationTest({
-  projectName: "ignore-missing",
-  shouldProduceSnapshots: true,
-})
--- integration-tests/unexpected-patch-creation-failure/unexpected-patch-creation-failure.test.ts
+++
@@ -1,6 +0,0 @@
-import { runIntegrationTest } from "../runIntegrationTest"
-runIntegrationTest({
-  projectName: "unexpected-patch-creation-failure",
-  exitCode: 1,
-  shouldProduceSnapshots: false,
-})
